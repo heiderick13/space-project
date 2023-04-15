ef575065a3838855baa11294d3475d2bfb358bc7
Marcelo Oliveira <marceloheiderick@outlook.com>
2023-04-15 12:07:15 -0300

    :lipstick: home page ok :white_check_mark:

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,31 +1,39 @@
+import { Link } from "react-router-dom";
+
 import "./Home.css";
 
 import background from "../../assets/img/background-home-desktop.jpg";
 
+import Container from "../../components/Container/Container";
+
 function Home() {
   return (
     <div
-      className="home"
+      className="home center"
       style={{ backgroundImage: `url(${background})`, minHeight: "100vh" }}
     >
       <div className="container">
-        <div className="row">
-          <div className="col-md-6 presentation">
-            <p className="sub-title">So, you want to travel to</p>
-            <h1 className="title">Space</h1>
-            <p className="trip-description">
-              Let’s face it; if you want to go to space, you might as well
-              genuinely go to outer space and not hover kind of on the edge of
-              it. Well sit back, and relax because we’ll give you a truly out of
-              this world experience!
-            </p>
-          </div>
-          <div className="col-md-6 wrapper">
-            <a href="./src/views/destinations.html" className="explore">
-              Explore
-            </a>
-          </div>
-        </div>
+        <Container
+          leftContent={
+            <div className="presentation">
+              <p className="sub-title">So, you want to travel to</p>
+              <h1 className="title">Space</h1>
+              <p className="trip-description">
+                Let’s face it; if you want to go to space, you might as well
+                genuinely go to outer space and not hover kind of on the edge of
+                it. Well sit back, and relax because we’ll give you a truly out
+                of this world experience!
+              </p>
+            </div>
+          }
+          rightContent={
+            <div className=" wrapper">
+              <Link to="/destinations" className="explore">
+                Explore
+              </Link>
+            </div>
+          }
+        />
       </div>
     </div>
   );
