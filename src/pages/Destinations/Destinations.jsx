104f578a588d85a8c46b86c20e5b222abfbdcb23
Marcelo Oliveira <marceloheiderick@outlook.com>
2023-04-29 11:23:05 -0300

    test new format

--- src/pages/Destinations/Destinations.jsx
+++ src/pages/Destinations/Destinations.jsx
@@ -1,9 +1,9 @@
 import background from "../../assets/img/background-destination-desktop.jpg";
 
-import moon from "../../assets/img/image-moon.png";
-import mars from "../../assets/img/image-mars.png";
-import europa from "../../assets/img/image-europa.png";
-import titan from "../../assets/img/image-titan.png";
+import moonImg from "../../assets/img/image-moon.png";
+import marsImg from "../../assets/img/image-mars.png";
+import europaImg from "../../assets/img/image-europa.png";
+import titanImg from "../../assets/img/image-titan.png";
 
 import Slider from "../../components/Slider/Slider";
 import Destination from "../../components/Destination/Destination";
@@ -11,6 +11,61 @@ import Destination from "../../components/Destination/Destination";
 // import Carousel from "react-bootstrap/Carousel";
 
 function Destinations() {
+  const moon = (
+    <Destination
+      key={1}
+      img={moonImg}
+      title={"Moon"}
+      description={`See our planet as you’ve never seen it before. A perfect
+  relaxing trip away to help regain perspective and come back
+  refreshed. While you’re there, take in some history by
+  visiting the Luna 2 and Apollo 11 landing sites.`}
+      distance={"384,400"}
+      travelTime={"3 days"}
+    />
+  );
+
+  const mars = (
+    <Destination
+      key={2}
+      img={marsImg}
+      title={"Mars"}
+      description={`Don’t forget to pack your hiking boots. You’ll need them to
+  tackle Olympus Mons, the tallest planetary mountain in our solar
+  system. It’s two and a half times the size of Everest!`}
+      distance={"225 mil."}
+      travelTime={"9 months"}
+    />
+  );
+
+  const europa = (
+    <Destination
+      key={3}
+      img={europaImg}
+      title={"Europa"}
+      description={`The smallest of the four Galilean moons orbiting Jupiter, Europa
+  is a winter lover’s dream. With an icy surface, it’s perfect for
+  a bit of ice skating, curling, hockey, or simple relaxation in
+  your snug wintery cabin.`}
+      distance={"628 mil."}
+      travelTime={"3 years"}
+    />
+  );
+
+  const titan = (
+    <Destination
+      key={4}
+      img={titanImg}
+      title={"Titan"}
+      description={`The only moon known to have a dense atmosphere other than Earth,
+  Titan is a home away from home (just a few hundred degrees
+  colder!). As a bonus, you get striking views of the Rings of
+  Saturn.`}
+      distance={"1.6 bil."}
+      travelTime={"7 years"}
+    />
+  );
+
   return (
     <>
       <div
@@ -22,49 +77,7 @@ function Destinations() {
         }}
       >
         <p className="page-title">01 Pick your destination</p>
-        <Slider
-          items={[
-            <Destination
-              img={moon}
-              title={"Moon"}
-              description={`See our planet as you’ve never seen it before. A perfect
-              relaxing trip away to help regain perspective and come back
-              refreshed. While you’re there, take in some history by
-              visiting the Luna 2 and Apollo 11 landing sites.`}
-              distance={"384,400"}
-              travelTime={"3 days"}
-            />,
-            <Destination
-              img={mars}
-              title={"Mars"}
-              description={`Don’t forget to pack your hiking boots. You’ll need them to
-              tackle Olympus Mons, the tallest planetary mountain in our solar
-              system. It’s two and a half times the size of Everest!`}
-              distance={"225 mil."}
-              travelTime={"9 months"}
-            />,
-            <Destination
-              img={europa}
-              title={"Europa"}
-              description={`The smallest of the four Galilean moons orbiting Jupiter, Europa
-              is a winter lover’s dream. With an icy surface, it’s perfect for
-              a bit of ice skating, curling, hockey, or simple relaxation in
-              your snug wintery cabin.`}
-              distance={"628 mil."}
-              travelTime={"3 years"}
-            />,
-            <Destination
-              img={titan}
-              title={"Titan"}
-              description={`The only moon known to have a dense atmosphere other than Earth,
-              Titan is a home away from home (just a few hundred degrees
-              colder!). As a bonus, you get striking views of the Rings of
-              Saturn.`}
-              distance={"1.6 bil."}
-              travelTime={"7 years"}
-            />,
-          ]}
-        />
+        <Slider items={[moon, mars, europa, titan]} />
       </div>
     </>
   );
